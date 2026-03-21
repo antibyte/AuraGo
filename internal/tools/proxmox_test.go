b16052b99a549263244cf53532ca354ca60302ae
antibyte <andreasjakob@gmx.de>
2026-03-21 20:24:54 +0100

    Harden Proxmox API client TLS handling

---
+++ internal/tools/proxmox_test.go
@@ -0,0 +1,28 @@
+package tools
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestGetProxmoxClient_CacheIsolatedByInsecureFlag(t *testing.T) {
+	proxmoxClientCache = sync.Map{}
+
+	secureClient := getProxmoxClient(ProxmoxConfig{Insecure: false})
+	insecureClient := getProxmoxClient(ProxmoxConfig{Insecure: true})
+
+	if secureClient == insecureClient {
+		t.Fatal("expected secure and insecure clients to be different instances")
+	}
+}
+
+func TestProxmoxRequest_RejectsNonHTTPSURL(t *testing.T) {
+	_, _, err := proxmoxRequest(ProxmoxConfig{
+		URL:     "http://example.com:8006",
+		TokenID: "user@pam!token",
+		Secret:  "secret",
+	}, "GET", "/nodes", "")
+	if err == nil {
+		t.Fatal("expected non-https URL to be rejected")
+	}
+}
